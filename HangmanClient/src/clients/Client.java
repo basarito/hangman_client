02ed8f1092004dadc5ebfddbf7ffaa5a251d13ea
borrovnica <anabasaric95@gmail.com>
2017-08-13 18:52:18 +0200

    Handled can't connect to server exception.

--- HangmanClient/src/clients/Client.java
+++ HangmanClient/src/clients/Client.java
@@ -70,7 +70,7 @@ public static void main(String[] args) {
 			}
 					
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("Can't connect to server, it appears to be down.");
 		}
 						
 	}
